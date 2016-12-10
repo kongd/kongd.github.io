aeda2f07000b49f2f1ace86296db14b5c8ee8618
dkong <kong.x.davidx@gmail.com>
2016-12-10 18:02:11 -0500

    more simple design

--- js/demo.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles'), {
-    dotColor: '#33ccff',
-    lineColor: '#33ccff',
-    parallaxMultiplier: '10',
-  });
-  var intro = document.getElementById('intro');
-  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
-}, false);
